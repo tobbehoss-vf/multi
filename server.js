1f7a9c8fee6db892f79c9a95468e226e8bc455e4
tobbehoss-vf <tobbehoss@gmail.com>
2026-02-26 11:32:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ class Projectile {
   constructor(x, y, angle, playerId, playerName, teamIndex) {
     this.x = x;
     this.y = y;
-    this.vx = Math.cos(angle) * 8; // Sänkt från 15
-    this.vy = Math.sin(angle) * 8;
+    this.vx = Math.cos(angle) * 40; // Sänkt från 15
+    this.vy = Math.sin(angle) * 40;
     this.playerId = playerId;
     this.playerName = playerName;
     this.teamIndex = teamIndex;
